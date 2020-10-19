1f72eb4c09deac33286f0ff9cd629a43f2eb8b50
alangburl <alangburl@gmail.com>
2020-10-18 19:36:41 -0500

    Add power method

---
+++ HW4/test2.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def power_iteration(A, num_simulations: int):
+    # Ideally choose a random vector
+    # To decrease the chance that our vector
+    # Is orthogonal to the eigenvector
+    b_k = np.array([1,.3,0])
+    f=open('ouput.tex','w')
+    i=0
+    for _ in range(num_simulations):
+        # calculate the matrix-by-vector product Ab
+        b_k1 = np.dot(A, b_k)
+        print(b_k1)
+        # calculate the norm
+        b_k1_norm = np.linalg.norm(b_k1)
+        print(b_k1_norm)
+        # re normalize the vector
+        g=r'{} & $\begin[pmatrix] {:.4f}\\{:.4f}\\{:.4f}\end[pmatrix]$ & {:.4f}\\'.format(i,b_k[0],b_k[1],b_k[2],b_k1_norm)
+        g=g.replace('[','{').replace(']','}')
+        f.write(g)
+        b_k = b_k1 / b_k1_norm
+
+        i+=1
+    f.close()
+    return b_k
+b=[[3, 5,3], [0, 4,6],[0,0,1]]
+a=power_iteration(np.array(b), 27)
